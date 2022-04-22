3dcf69627627d6a73fb92963298ce3189c9efd35
JacksonHagen <jackson.d.hagen@gmail.com>
2022-04-22 13:15:14 -0600

    Test

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,30 +1,30 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 export const CommentSchema = new Schema(
-    {
-        body: { type: String, required: true },
-        accountId: { type: Schema.Types.ObjectId, required: true },
-        postId: { type: Schema.Types.ObjectId, required: true }
-    },
-    {
-        timestamps: true,
-        toJSON: { virtuals: true }
-    }
+  {
+    body: { type: String, required: true },
+    accountId: { type: Schema.Types.ObjectId, required: true },
+    postId: { type: Schema.Types.ObjectId, required: true }
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true }
+  }
 )
 
-CommentSchema.virtual("account",
-    {
-        localField: "accountId",
-        foreignField: "_id",
-        ref: "Account",
-        justOne: true
-    })
+CommentSchema.virtual('account',
+  {
+    localField: 'accountId',
+    foreignField: '_id',
+    ref: 'Account',
+    justOne: true
+  })
 
-CommentSchema.virtual("post",
-    {
-        localField: "postId",
-        foreignField: "_id",
-        ref: "Post",
-        justOne: true
-    })
+CommentSchema.virtual('post',
+  {
+    localField: 'postId',
+    foreignField: '_id',
+    ref: 'Post',
+    justOne: true
+  })
--- server/services/CommentsService.js
+++ server/services/CommentsService.js
@@ -1,19 +1,24 @@
-class CommentsService
-{
-    async create(body)
-    {
+import { dbContext } from '../db/DbContext.js'
+import { BadRequest, Forbidden } from '../utils/Errors.js'
 
+class CommentsService {
+  async create(body) {
+    return await dbContext.Comments.create(body)
+  }
+
+  async edit(update) {
+    const original = await dbContext.Comments.findById(update.id)
+    if(!original) {
+        throw new BadRequest("No comment with that ID was found")
     }
-
-    async edit(update)
-    {
-
+    if(original.accountId.toString !== update.accountId) {
+        throw new Forbidden("No ")
     }
+  }
 
-    async remove(id, userId)
-    {
-        
-    }
+  async remove(id, userId) {
+
+  }
 }
 
-export const commentsService = new CommentsService();
+export const commentsService = new CommentsService()
